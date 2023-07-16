89307a959511c5841cf68a79bcda9fe629b6e43a
rahulkumar-312 <125283602+rahulkumar-312@users.noreply.github.com>
2023-07-16 13:10:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const numberSet = "1234567890"
+const symbolSet = "~!@#$%^&*()_+/"
+
+// selectors
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+
+
+
+
+
+const randomData = (dataSet) =>{
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+
+}
+
+const generatePassword = (password = "") => {
+    if(upperInput.checked){
+        password += randomData(upperCase);
+    }
+    if(lowerInput.checked){
+        password += randomData(lowerCase);
+    }
+    if(numberInput.checked){
+        password += randomData(numberSet);
+    }
+    if(symbolInput.checked){
+        password += randomData(symbolSet);
+    }
+    if(password.length < totalChar.value){
+        return generatePassword(password);
+    }
+    
+    passBox.innerText = truncateString(password, totalChar.value);
+
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+    function(){
+        generatePassword();
+    }
+)
+
+function truncateString(str, num){
+    if(str.length > num){
+        let subStr = str.substring(0,num);
+        return subStr;
+    } else{
+        return str;
+    }
+}
+
+generatePassword();
